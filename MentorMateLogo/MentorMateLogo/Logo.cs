3c7b7e836185d4b5d41a77977f3e7f7f3ed219d0
mariyap <mariqpetkova98@abv.bg>
2020-02-12 19:48:14 +0200

    initial commit

---
+++ MentorMateLogo/MentorMateLogo/Logo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorMateLogo
+{
+    class Logo
+    {
+        public int N { protected set; get; }
+        public char[,] MMLogo { protected set; get; }
+        
+        public Logo(int n)
+        {
+     
+            this.N = n;
+
+            //array contains only one M
+            MMLogo = new char[N + 1, N * 5];
+        }
+
+
+        //Draw function to fill the array
+        public void Draw()
+        {
+            int temp = N;
+            bool switcher = true;
+
+            //populating the array with '-'
+            for (int i=0; i <N+1;i++)
+            {
+                for(int j=0; j<N*5; j++)
+                {
+                    MMLogo[i, j] = '-';
+                }
+            }
+ 
+           //switching the '-' to '*'
+           for(int i = 0; i<=N*4; i++)
+           {
+                for(int k=i; k<N+i; k++)
+                { 
+                    MMLogo[temp, k] = '*';
+                }
+
+                if(temp==0)
+                {
+                    switcher = false;
+                }
+
+                if(temp==N)
+                {
+                    switcher = true;
+
+                }
+
+                if (switcher)
+                {
+                    --temp;
+                }
+                else
+                {
+                    ++temp;
+                }
+
+            }
+      
+                    
+        }
+
+        //Print function to print the array
+        public void Print()
+        {
+            for(int i=0; i<N+1; i++)
+            {
+                //to print the same array twice
+                for(int count = 0; count <2; count++)
+                {
+                    for(int j=0; j<N*5; j++)
+                    {
+                        Console.Write(MMLogo[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ MentorMateLogo/MentorMateLogo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorMateLogo
+{
+    class Program
+    {
+        //Input function and validation
+        static int EnterN()
+        {
+
+            Console.WriteLine("N must be between 2 and 10 000; Must be an odd number;");
+            Console.WriteLine("Enter N:");
+
+            int n;
+            bool validation;
+
+            do
+            {
+                n = Int32.Parse(Console.ReadLine());
+
+                validation = (n > 2) && (n < 10000) && (n % 2 != 0);
+
+                if (validation == false)
+                {
+                    Console.WriteLine("Invalid input; Please enter again:");
+                    continue;
+                }
+
+
+            }
+            while (validation == false);
+
+            return n;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = EnterN();
+            Logo logo = new Logo(n);
+            logo.Draw();
+            logo.Print();
+
+
+            //Exit program
+            Console.ReadKey();
+        }
+    }
+}
